a7dbfb69f0d62eafa58981a975a7b3968aa6c9b6
preetimgehlot <36564500+preetimgehlot@users.noreply.github.com>
2020-08-25 23:46:43 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,14 @@
+import os
+import socket
+from flask import Flask
+
+app = Flask(__name__)
+
+hostname = socket.gethostname()
+host = socket.gethostbyname(hostname)
+
+@app.route("/")
+def index():
+    return "Hello World !!!"
+
+app.run(host=host, port=8080)
